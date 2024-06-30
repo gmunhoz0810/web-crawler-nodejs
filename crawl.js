900438c47547c5d0cde19b9c5b35b0d57a1dc0a0
gmunhoz0810 <gabrielmunhoz12345@gmail.com>
2024-06-30 06:17:22 -0400

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,6 +1,19 @@
 import { JSDOM } from 'jsdom'
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
     console.log(`activelly crawling: ${currentURL}`)
 
     try {
@@ -8,19 +21,27 @@ async function crawlPage(currentURL) {
 
         if (resp.status > 399) {
             console.log(`error in fetch with satus code: ${resp.status} on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")) {
             console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
